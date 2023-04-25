d023f17f40697a88fc84343654e3246201a800cd
yilma <yilma@DESKTOP-U1LH654>
2023-04-25 21:37:56 +0300

    Add project files.

---
+++ 250423_NBUY3/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace _250423_NBUY3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListe = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.urunlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gıdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temizlikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kategorilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudFiyat = new System.Windows.Forms.NumericUpDown();
+            this.nudStok = new System.Windows.Forms.NumericUpDown();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtKategoriAdi = new System.Windows.Forms.TextBox();
+            this.txtTanimi = new System.Windows.Forms.TextBox();
+            this.btnKategoriEkle = new System.Windows.Forms.Button();
+            this.faturalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStok)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListe
+            // 
+            this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListe.Location = new System.Drawing.Point(12, 107);
+            this.dgvListe.Name = "dgvListe";
+            this.dgvListe.Size = new System.Drawing.Size(776, 260);
+            this.dgvListe.TabIndex = 0;
+            this.dgvListe.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListe_CellClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.urunlerToolStripMenuItem,
+            this.kategorilerToolStripMenuItem,
+            this.faturalarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // urunlerToolStripMenuItem
+            // 
+            this.urunlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gıdaToolStripMenuItem,
+            this.temizlikToolStripMenuItem});
+            this.urunlerToolStripMenuItem.Name = "urunlerToolStripMenuItem";
+            this.urunlerToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.urunlerToolStripMenuItem.Text = "Urunler";
+            this.urunlerToolStripMenuItem.Click += new System.EventHandler(this.urunlerToolStripMenuItem_Click);
+            // 
+            // gıdaToolStripMenuItem
+            // 
+            this.gıdaToolStripMenuItem.Name = "gıdaToolStripMenuItem";
+            this.gıdaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gıdaToolStripMenuItem.Text = "Gıda";
+            // 
+            // temizlikToolStripMenuItem
+            // 
+            this.temizlikToolStripMenuItem.Name = "temizlikToolStripMenuItem";
+            this.temizlikToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.temizlikToolStripMenuItem.Text = "Temizlik";
+            // 
+            // kategorilerToolStripMenuItem
+            // 
+            this.kategorilerToolStripMenuItem.Name = "kategorilerToolStripMenuItem";
+            this.kategorilerToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.kategorilerToolStripMenuItem.Text = "Kategoriler";
+            this.kategorilerToolStripMenuItem.Click += new System.EventHandler(this.kategorilerToolStripMenuItem_Click);
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(78, 38);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(132, 20);
+            this.txtUrunAdi.TabIndex = 2;
+            this.txtUrunAdi.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UrunAdi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fiyat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Stok";
+            // 
+            // nudFiyat
+            // 
+            this.nudFiyat.Location = new System.Drawing.Point(257, 38);
+            this.nudFiyat.Name = "nudFiyat";
+            this.nudFiyat.Size = new System.Drawing.Size(120, 20);
+            this.nudFiyat.TabIndex = 6;
+            this.nudFiyat.Visible = false;
+            // 
+            // nudStok
+            // 
+            this.nudStok.Location = new System.Drawing.Point(412, 38);
+            this.nudStok.Name = "nudStok";
+            this.nudStok.Size = new System.Drawing.Size(120, 20);
+            this.nudStok.TabIndex = 7;
+            this.nudStok.Visible = false;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(566, 36);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(88, 20);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Visible = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtKategoriAdi
+            // 
+            this.txtKategoriAdi.Location = new System.Drawing.Point(78, 81);
+            this.txtKategoriAdi.Name = "txtKategoriAdi";
+            this.txtKategoriAdi.Size = new System.Drawing.Size(132, 20);
+            this.txtKategoriAdi.TabIndex = 9;
+            this.txtKategoriAdi.Visible = false;
+            // 
+            // txtTanimi
+            // 
+            this.txtTanimi.Location = new System.Drawing.Point(257, 81);
+            this.txtTanimi.Name = "txtTanimi";
+            this.txtTanimi.Size = new System.Drawing.Size(120, 20);
+            this.txtTanimi.TabIndex = 10;
+            this.txtTanimi.Visible = false;
+            // 
+            // btnKategoriEkle
+            // 
+            this.btnKategoriEkle.Location = new System.Drawing.Point(566, 81);
+            this.btnKategoriEkle.Name = "btnKategoriEkle";
+            this.btnKategoriEkle.Size = new System.Drawing.Size(88, 20);
+            this.btnKategoriEkle.TabIndex = 11;
+            this.btnKategoriEkle.Text = "Ekle";
+            this.btnKategoriEkle.UseVisualStyleBackColor = true;
+            this.btnKategoriEkle.Visible = false;
+            this.btnKategoriEkle.Click += new System.EventHandler(this.btnKategoriEkle_Click);
+            // 
+            // faturalarToolStripMenuItem
+            // 
+            this.faturalarToolStripMenuItem.Name = "faturalarToolStripMenuItem";
+            this.faturalarToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.faturalarToolStripMenuItem.Text = "Faturalar";
+            this.faturalarToolStripMenuItem.Click += new System.EventHandler(this.faturalarToolStripMenuItem_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(678, 81);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(90, 20);
+            this.btnGuncelle.TabIndex = 12;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Visible = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKategoriEkle);
+            this.Controls.Add(this.txtTanimi);
+            this.Controls.Add(this.txtKategoriAdi);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.nudStok);
+            this.Controls.Add(this.nudFiyat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.dgvListe);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStok)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListe;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem urunlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gıdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temizlikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kategorilerToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudFiyat;
+        private System.Windows.Forms.NumericUpDown nudStok;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtKategoriAdi;
+        private System.Windows.Forms.TextBox txtTanimi;
+        private System.Windows.Forms.Button btnKategoriEkle;
+        private System.Windows.Forms.ToolStripMenuItem faturalarToolStripMenuItem;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ 250423_NBUY3/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _250423_NBUY3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Server=.;Database=Northwind;User=sa;Pwd=123");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UrunListele();
+            label2.Visible = true;
+            label3.Visible = true;
+            label1.Visible = true;
+            txtUrunAdi.Visible = true;
+            nudFiyat.Visible = true;
+            nudStok.Visible = true;
+            btnEkle.Visible = true;
+            btnGuncelle.Visible = true;
+        }
+        private void UrunListele()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Urunler", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dgvListe.DataSource = dt;
+            dgvListe.Columns["UrunId"].Visible = false;
+            dgvListe.Columns["KategoriID"].Visible = false;
+            dgvListe.Columns["TedarikciID"].Visible = false;
+        }
+        private void urunlerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UrunListele();
+            label2.Visible = true;
+            label3.Visible = true;
+            label1.Visible = true;
+            txtUrunAdi.Visible = true;
+            nudFiyat.Visible = true;
+            nudStok.Visible = true;
+            txtKategoriAdi.Visible = false;
+            txtTanimi.Visible = false;
+            btnKategoriEkle.Visible = false;
+            btnEkle.Visible = true;
+        }
+        private void kategorilerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KategoriListele();
+            label2.Visible = false;
+            label3.Visible = false;
+            label1.Visible = false;
+            txtUrunAdi.Visible = false;
+            nudFiyat.Visible = false;
+            nudStok.Visible = false;
+            txtKategoriAdi.Visible = true;
+            txtTanimi.Visible = true;
+            btnKategoriEkle.Visible = true;
+            btnEkle.Visible = false;
+        }
+        private void KategoriListele()
+        {
+            dgvListe.RowTemplate.Height = 150;
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Kategoriler", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dgvListe.DataSource = dt;
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string adi = txtUrunAdi.Text;
+            decimal fiyat = nudFiyat.Value;
+            decimal stok = nudStok.Value;
+            if (adi == string.Empty || fiyat == 0 || stok == 0)
+            {
+                MessageBox.Show("Lütfen alanları doldurun.");
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = string.Format("insert urunler(UrunAdi,BirimFiyati,HedefStokDuzeyi) values ('{0}',{1},{2})", adi, fiyat, stok);
+                cmd.Connection = baglanti;
+                baglanti.Open();
+                int etk = cmd.ExecuteNonQuery();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Kayıt Eklendi.");
+                    UrunListele();
+                }
+                else
+                {
+                    MessageBox.Show("Hata.");
+                }
+                baglanti.Close();
+                txtUrunAdi.Text = string.Empty;
+                nudFiyat.Value = 0;
+                nudStok.Value = 0;
+            }
+        }
+
+        private void btnKategoriEkle_Click(object sender, EventArgs e)
+        {
+            string adi = txtKategoriAdi.Text;
+            string tanimi = txtTanimi.Text;
+            if (adi == string.Empty || tanimi == string.Empty)
+            {
+                MessageBox.Show("Lütfen alanları doldurun.");
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = string.Format("insert kategoriler(KategoriAdi,Tanimi) values ('{0}','{1}')", adi, tanimi);
+                cmd.Connection = baglanti;
+                baglanti.Open();
+                int etk = cmd.ExecuteNonQuery();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Kayıt Eklendi.");
+                    KategoriListele();
+                }
+                else
+                {
+                    MessageBox.Show("Hata.");
+                }
+                baglanti.Close();
+                txtKategoriAdi.Text = string.Empty;
+                txtTanimi.Text = string.Empty;
+            }
+        }
+
+        private void faturalarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Faturalar", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dgvListe.DataSource = dt;
+            label2.Visible = false;
+            label3.Visible = false;
+            label1.Visible = false;
+            txtUrunAdi.Visible = false;
+            nudFiyat.Visible = false;
+            nudStok.Visible = false;
+            txtKategoriAdi.Visible = false;
+            txtTanimi.Visible = false;
+            btnKategoriEkle.Visible = false;
+        }
+
+        private void dgvListe_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtUrunAdi.Tag = dgvListe.CurrentRow.Cells["UrunID"].Value;
+            txtUrunAdi.Text = dgvListe.CurrentRow.Cells["UrunAdi"].Value.ToString();
+            nudFiyat.Value = (decimal)dgvListe.CurrentRow.Cells["BirimFiyati"].Value;
+            nudStok.Value = Convert.ToInt32(dgvListe.CurrentRow.Cells["HedefStokDuzeyi"].Value);
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string adi = txtUrunAdi.Text;
+            int fiyat = (int)nudFiyat.Value;
+            decimal stok = nudStok.Value;
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = string.Format("update urunler set UrunAdi='{0}',BirimFiyati={1},HedefStokDuzeyi={2} where UrunID={3}", adi, fiyat, stok, txtUrunAdi.Tag);
+            komut.Connection = baglanti;
+            baglanti.Open();
+            try
+            {
+                int etk = komut.ExecuteNonQuery();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Güncellendi.");
+                    UrunListele();
+                }
+                else
+                {
+                    MessageBox.Show("Güncelleme Başarısız.");
+                }
+
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+    }
+}
+
